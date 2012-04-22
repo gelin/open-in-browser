0e8c030ba50333e2778a21e9ae0b693a6e03aaef
Denis Nelubin <dnelubin@gmail.com>
2012-04-22 18:38:25 +0700

    Investigating file managers' intents.

--- src/ru/gelin/android/browser/open/OpenBrowserActivity.java
+++ src/ru/gelin/android/browser/open/OpenBrowserActivity.java
@@ -1,9 +1,17 @@
 package ru.gelin.android.browser.open;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  *  Converts input view intent into browsable intent.
@@ -12,11 +20,34 @@ public class OpenBrowserActivity extends Activity {
     
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(getIntent());
+        Intent intent = getIntent();
         Log.d(Tag.TAG, "Intent: " + intent);
-        intent.addCategory(Intent.CATEGORY_BROWSABLE);
-        startActivity(Intent.createChooser(intent, getString(R.string.open_in_browser)));
+        Uri uri = intent.getData();
+        if (uri == null) {
+            finish();
+            return;
+        } else if ("content".equals(uri.getScheme())) {
+            startActivityForContent(uri);
+        }
         finish();
     }
-    
+
+    void startActivityForContent(Uri uri) {
+        ContentResolver resolver = getContentResolver();
+        Cursor cursor = resolver.query(uri, null, null, null, null);
+        Log.d(Tag.TAG, "Columns: " + Arrays.asList(cursor.getColumnNames()));
+        cursor.moveToFirst();
+        Uri newUri = Uri.parse(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA)));
+        Intent intent = new Intent(Intent.ACTION_VIEW, newUri);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(Intent.createChooser(intent, getString(R.string.open_in_browser)));
+    }
+
+    //TODO: process sifferent content types
+
+    //dat=content://com.metago.astro.filesystem/mnt/sdcard/books/mongo/DOCS/1.0 Changelist.html typ=text/html
+    //dat=content://media/external/file/15271 typ=text/html
+
+    //TODO: open only in browsers
+
 }
