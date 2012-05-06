d1dd24ad1a2e6ceb5b569eed0d58b66d616d54b7
Denis Nelubin <dnelubin@gmail.com>
2012-05-06 16:30:27 +0700

    Made main activity useful.

--- src/ru/gelin/android/browser/open/MainActivity.java
+++ src/ru/gelin/android/browser/open/MainActivity.java
@@ -2,14 +2,22 @@ package ru.gelin.android.browser.open;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        View main = findViewById(R.id.main);
+        main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
+
 }
